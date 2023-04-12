5eb0d65752909b294d87280c246e5d3c931b982f
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-04-12 18:15:40 +0200

    oh-my-zsh + powerlevel10k

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 ln -sf ~/dotfiles/src/git/gitignore ~/.gitignore
+
+
+ln -s ~/dotfiles/src/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/dotfiles/src/zsh/p10k.zsh ~/.p10k.zsh
+ln -sf ~/dotfiles/src/zsh/zshrc ~/.zshrc
