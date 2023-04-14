5528a8e9e94327c128751422226a66e678d1b8dd
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-04-14 10:37:33 +0200

    Submodule corrections

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -2,7 +2,7 @@
 
 ln -sf ~/dotfiles/src/git/gitignore ~/.gitignore
 
-
+ln -s ~/dotfiles/src/zsh/powerlevel10k ~/dotfiles/src/zsh/oh-my-zsh/custom/themes/powerlevel10k 
 ln -s ~/dotfiles/src/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/src/zsh/p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/src/zsh/zshrc ~/.zshrc
